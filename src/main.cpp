eb00d1a36be92f231da3728ac0621c1f83effdf9
antho <anthonyzolmora@outlook.com>
2025-02-05 01:17:01 -0700

    changed name

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,8 @@
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
+    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "Adventure of Slime");
+    window.setFramerateLimit(60);
 
     while (window.isOpen())
     {
