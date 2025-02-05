e7493c710e6f946879fecca031019e0a746cd8ba
antho <anthonyzolmora@outlook.com>
2025-02-05 11:42:31 -0700

    fixed slime class

--- src/slime.h
+++ src/slime.h
@@ -1,23 +1,20 @@
-//
 // Created by antho on 2/5/2025.
-//
 
 #ifndef SLIME_H
 #define SLIME_H
+
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
 class slime {
 public:
     void idle() {
-        if (!idle0.loadFromFile(idle0FileName, false, sf::IntRect({10, 10}, {19, 15}))) {
+        if (!idle0.loadFromFile(idle0FileName)) {
             std::cout << "Cannot load image idle0 \n";
         }
-        sf::Sprite idle0Sprite(idle0);
-        idle0Sprite.setTexture(idle0); // Set initial texture
-        //Define the texture rectangle using FloatRect and convert to IntRect
-        sf::FloatRect floatRect(sf::Vector2f(0.f, 0.f), sf::Vector2f(19.f, 15.f));
-        idle0Sprite.setTextureRect(sf::IntRect(floatRect));
+        idle0Sprite.setTexture(idle0);
+        // Define the texture rectangle using IntRect
+        idle0Sprite.setTextureRect(sf::IntRect({10, 10}, {19, 15}));
     }
 
     void draw(sf::RenderWindow &window) {
@@ -26,8 +23,8 @@ public:
 
 private:
     sf::Texture idle0;
+    sf::Sprite idle0Sprite;
     std::string idle0FileName = "assets/slime/Idle/0.png";
 };
-
 
 #endif //SLIME_H
