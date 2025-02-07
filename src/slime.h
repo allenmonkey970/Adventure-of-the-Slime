18cad8fa3e33da1d3ef0c5b85d6c94d593273d60
antho <anthonyzolmora@outlook.com>
2025-02-07 00:19:41 -0700

    removing swhallow attack for now and replace with hit

--- src/slime.h
+++ src/slime.h
@@ -7,16 +7,17 @@ class Slime {
 public:
     Slime() : sprite(texture) {
         // Load texture and set up animations
-        if (!texture.loadFromFile("assets/slime/Swallow/swallow.png")) {
+        if (!texture.loadFromFile("assets/slime/hit/damage.png")) {
             std::cerr << "Failed to load texture\n";
         } else {
             sprite.setTexture(texture); // Set the texture to the sprite
-            AnimationManager::addAnimation("swallow", texture, {14, 1}, {80, 72}, {0, 0}, 4);
+            AnimationManager::addAnimation("hit", texture, {7, 1}, {32, 32}, {0, 0}, 7);
+            sprite.setScale({2, 2});
         }
     }
 
     void draw(sf::RenderWindow& window) {
-        AnimationManager::update("swallow", sprite);
+        AnimationManager::update("hit", sprite);
         window.draw(sprite);
     }
 
