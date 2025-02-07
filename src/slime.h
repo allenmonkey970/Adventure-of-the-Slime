e72f21e932a77c9ab9f45d6cf7dee68e486624f4
antho <anthonyzolmora@outlook.com>
2025-02-06 23:14:45 -0700

    created idle animation and change idle pngs to spritesheet

--- src/slime.h
+++ src/slime.h
@@ -7,11 +7,11 @@ class Slime {
 public:
     Slime() : sprite(texture) {
         // Load texture and set up animations
-        if (!texture.loadFromFile("assets/idle.png")) {
+        if (!texture.loadFromFile("assets/slime/Idle/idle.png")) {
             std::cerr << "Failed to load texture\n";
         } else {
             sprite.setTexture(texture); // Set the texture to the sprite
-            AnimationManager::addAnimation("idle", texture, {3, 2}, {21, 19}, {0, 0}, 10);
+            AnimationManager::addAnimation("idle", texture, {3, 2}, {21, 19}, {0, 0}, 7);
 
             sprite.setScale(sf::Vector2f(2.0f, 2.0f));
         }
