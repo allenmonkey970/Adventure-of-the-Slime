cfb1d75e3db19ba3618a02979f35584c57a04e9c
antho <anthonyzolmora@outlook.com>
2025-02-06 23:55:47 -0700

    editing assets

--- src/slime.h
+++ src/slime.h
@@ -7,13 +7,11 @@ class Slime {
 public:
     Slime() : sprite(texture) {
         // Load texture and set up animations
-        if (!texture.loadFromFile("assets/slime/Idle/idle.png")) {
+        if (!texture.loadFromFile("assets/slime/Swallow/swallow.png")) {
             std::cerr << "Failed to load texture\n";
         } else {
             sprite.setTexture(texture); // Set the texture to the sprite
-            AnimationManager::addAnimation("idle", texture, {3, 2}, {21, 19}, {0, 0}, 7);
-
-            sprite.setScale(sf::Vector2f(2.0f, 2.0f));
+            AnimationManager::addAnimation("swallow", texture, {14, 1}, {80, 72}, {0, 0}, 4);
         }
     }
 
