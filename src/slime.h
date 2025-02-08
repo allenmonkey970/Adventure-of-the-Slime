230102484aa5da49e5f5f106ddafdd0fc19c9374
antho <anthonyzolmora@outlook.com>
2025-02-07 22:29:22 -0700

    formated slime class better

--- src/slime.h
+++ src/slime.h
@@ -6,32 +6,13 @@
 class Slime {
 public:
     Slime() : sprite(hitTexture) {
-        // Load texture and set up animations
-        if (!hitTexture.loadFromFile("assets/slime/hit/damage.png")) {
-            std::cerr << "Failed to load texture\n";
-        } else {
-            sprite.setTexture(hitTexture); // Set the texture to the sprite
-            AnimationManager::addAnimation("hit", hitTexture, {7, 1}, {40, 32}, {0, 0}, 3);
-            sprite.setScale({2, 2});
-        }
-        if (!idleTexture.loadFromFile("assets/slime/Idle/idle.png")) {
-            std::cerr << "Failed to load texture\n";
-        } else {
-            sprite.setTexture(idleTexture); // Set the texture to the sprite
-            AnimationManager::addAnimation("hit", idleTexture, {7, 1}, {30, 27}, {0, 0}, 3);
-            sprite.setScale({2, 2});
-        }
-        if (!deathTexture.loadFromFile("assets/slime/Death/death.png")) {
-            std::cerr << "Failed to load texture\n";
-        } else {
-            sprite.setTexture(deathTexture); // Set the texture to the sprite
-            AnimationManager::addAnimation("hit", deathTexture, {6, 1}, {55, 45}, {0, 0}, 2);
-            sprite.setScale({2, 2});
-        }
+        setupAnimation(hitTexture, "assets/slime/hit/damage.png", "hit", {7, 1}, {40, 32}, {0, 0}, 3);
+        setupAnimation(idleTexture, "assets/slime/Idle/idle.png", "idle", {7, 1}, {30, 27}, {0, 0}, 3);
+        setupAnimation(deathTexture, "assets/slime/Death/death.png", "death", {6, 1}, {55, 45}, {0, 0}, 2);
     }
 
     void draw(sf::RenderWindow& window) {
-        AnimationManager::update("hit", sprite);
+        AnimationManager::update("damage", sprite); // Update this line as needed to switch between animations
         window.draw(sprite);
     }
 
@@ -40,4 +21,13 @@ private:
     sf::Texture hitTexture;
     sf::Texture idleTexture;
     sf::Texture deathTexture;
+
+    void setupAnimation(sf::Texture& texture, const std::string& filePath, const std::string& animationName, sf::Vector2i frameCount, sf::Vector2i frameSize, sf::Vector2i startPosition, int speed) {
+        if (!texture.loadFromFile(filePath)) {
+            std::cerr << "Failed to load texture: " << filePath << "\n";
+        } else {
+            AnimationManager::addAnimation(animationName, texture, frameCount, frameSize, startPosition, speed);
+            sprite.setScale({2, 2});
+        }
+    }
 };
