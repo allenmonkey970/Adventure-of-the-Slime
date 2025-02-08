b8b1b15e3482dc16c93650f35285f3b68c73b471
antho <anthonyzolmora@outlook.com>
2025-02-07 22:39:44 -0700

    able to choose scale and which animation when using the slime class function setScale

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@ int main() {
     window.setFramerateLimit(30);
 
     Slime mainSlime;
+    mainSlime.setScale("hit", {3, 3});
     while (window.isOpen()) {
         while (const std::optional<sf::Event> event = window.pollEvent()) {
             if (event->is<sf::Event::Closed>()) {
--- src/slime.h
+++ src/slime.h
@@ -5,14 +5,27 @@
 
 class Slime {
 public:
-    Slime() : sprite(idleTexture) {
+    Slime() : sprite(idleTexture), currentAnimation("idle") {
         setupAnimation(hitTexture, "assets/slime/hit/damage.png", "hit", {7, 1}, {40, 32}, {0, 0}, 3);
         setupAnimation(idleTexture, "assets/slime/Idle/idle.png", "idle", {7, 1}, {30, 27}, {0, 0}, 3);
         setupAnimation(deathTexture, "assets/slime/Death/death.png", "death", {6, 1}, {55, 45}, {0, 0}, 2);
+        sprite.setTexture(idleTexture);
+    }
+
+    void setScale(const std::string& animationName, const sf::Vector2f& scale) {
+        if (animationName == "hit") {
+            sprite.setTexture(hitTexture);
+        } else if (animationName == "idle") {
+            sprite.setTexture(idleTexture);
+        } else if (animationName == "death") {
+            sprite.setTexture(deathTexture);
+        }
+        currentAnimation = animationName;
+        sprite.setScale(scale);
     }
 
     void draw(sf::RenderWindow& window) {
-        AnimationManager::update("damage", sprite); // Update this line as needed to switch between animations
+        AnimationManager::update(currentAnimation, sprite);
         window.draw(sprite);
     }
 
@@ -21,13 +34,13 @@ private:
     sf::Texture hitTexture;
     sf::Texture idleTexture;
     sf::Texture deathTexture;
+    std::string currentAnimation;
 
     void setupAnimation(sf::Texture& texture, const std::string& filePath, const std::string& animationName, sf::Vector2i frameCount, sf::Vector2i frameSize, sf::Vector2i startPosition, int speed) {
         if (!texture.loadFromFile(filePath)) {
             std::cerr << "Failed to load texture: " << filePath << "\n";
         } else {
             AnimationManager::addAnimation(animationName, texture, frameCount, frameSize, startPosition, speed);
-            sprite.setScale({2, 2});
         }
     }
 };
