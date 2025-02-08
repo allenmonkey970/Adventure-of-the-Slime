1a1e5822c718abf9ddbfd9f63a1777a381129e05
antho <anthonyzolmora@outlook.com>
2025-02-07 22:25:45 -0700

    put sprites in horizontal instead of compact layout for animation

--- src/slime.h
+++ src/slime.h
@@ -5,13 +5,27 @@
 
 class Slime {
 public:
-    Slime() : sprite(texture) {
+    Slime() : sprite(hitTexture) {
         // Load texture and set up animations
-        if (!texture.loadFromFile("assets/slime/hit/damage.png")) {
+        if (!hitTexture.loadFromFile("assets/slime/hit/damage.png")) {
             std::cerr << "Failed to load texture\n";
         } else {
-            sprite.setTexture(texture); // Set the texture to the sprite
-            AnimationManager::addAnimation("hit", texture, {7, 1}, {40, 32}, {0, 0}, 3);
+            sprite.setTexture(hitTexture); // Set the texture to the sprite
+            AnimationManager::addAnimation("hit", hitTexture, {7, 1}, {40, 32}, {0, 0}, 3);
+            sprite.setScale({2, 2});
+        }
+        if (!idleTexture.loadFromFile("assets/slime/Idle/idle.png")) {
+            std::cerr << "Failed to load texture\n";
+        } else {
+            sprite.setTexture(idleTexture); // Set the texture to the sprite
+            AnimationManager::addAnimation("hit", idleTexture, {7, 1}, {30, 27}, {0, 0}, 3);
+            sprite.setScale({2, 2});
+        }
+        if (!deathTexture.loadFromFile("assets/slime/Death/death.png")) {
+            std::cerr << "Failed to load texture\n";
+        } else {
+            sprite.setTexture(deathTexture); // Set the texture to the sprite
+            AnimationManager::addAnimation("hit", deathTexture, {6, 1}, {55, 45}, {0, 0}, 2);
             sprite.setScale({2, 2});
         }
     }
@@ -23,5 +37,7 @@ public:
 
 private:
     sf::Sprite sprite;
-    sf::Texture texture;
+    sf::Texture hitTexture;
+    sf::Texture idleTexture;
+    sf::Texture deathTexture;
 };
