9e9aa715d19c33948c6175a95c2cacd209d59b62
antho <anthonyzolmora@outlook.com>
2025-02-17 03:44:13 -0700

    update animation and added broken melee

--- src/slime.cpp
+++ src/slime.cpp
@@ -1,4 +1,37 @@
 #include "slime.h"
+#include <SFML/Window/Mouse.hpp>
+
+// Constructor for Slime class
+Slime::Slime() : sprite(idleTexture), currentAnimation("idle") {
+    setupAnimation(hitRightTexture, "hitRight", "assets/slime/hit/damageRight.png", {7, 1}, {40, 32}, {0, 0}, 6);
+    setupAnimation(hitLeftTexture, "hitLeft", "assets/slime/hit/damageRight.png", {7, 1}, {40, 32}, {0, 0}, 6);
+    setupAnimation(idleTexture, "idle", "assets/slime/Idle/idle.png", {7, 1}, {30, 27}, {0, 0}, 10);
+    setupAnimation(deathTexture, "death", "assets/slime/Death/death.png", {6, 1}, {55, 45}, {0, 0}, 4);
+
+    // Movement animations
+    setupAnimation(moveDownTexture, "moveDown", "assets/slime/walking/down/walkingDown.png", {3, 1}, {42, 42}, {0, 0}, 7);
+    setupAnimation(moveRightTexture, "moveRight", "assets/slime/walking/sideways/walkingRight.png", {7, 1}, {30, 28}, {0, 0}, 6);
+    setupAnimation(moveLeftTexture, "moveLeft", "assets/slime/walking/sideways/walkingLeft.png", {7, 1}, {30, 28}, {0, 0}, 6);
+    setupAnimation(moveUpTexture, "moveUp", "assets/slime/walking/up/walkingUpwards.png", {3, 1}, {43, 42}, {0, 0}, 7);
+    sprite.setTexture(idleTexture);
+}
+
+void Slime::draw(sf::RenderWindow &window) {
+    AnimationManager::update(currentAnimation, sprite);
+    window.draw(sprite);
+}
+
+void Slime::move(const sf::Vector2f &offset) {
+    sprite.move(offset);
+}
+
+void Slime::hit(const std::string &direction) {
+    if (direction == "left") {
+        currentAnimation = "hitLeft";
+    } else if (direction == "right") {
+        currentAnimation = "hitRight";
+    }
+}
 
 sf::Vector2f Slime::handleMovement(const TileMap& map) {
     try {
@@ -7,37 +40,56 @@ sf::Vector2f Slime::handleMovement(const TileMap& map) {
         constexpr float verticalMoveSpeed = 5.f;
         bool isMoving = false;
 
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
-            movement.x -= horizontalMoveSpeed;
-            if (currentAnimation != "moveLeft") {
-                currentAnimation = "moveLeft";
+        // Handle movement only if not attacking
+        if (!isAttacking) {
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
+                movement.x -= horizontalMoveSpeed;
+                if (currentAnimation != "moveLeft") {
+                    currentAnimation = "moveLeft";
+                }
+                isMoving = true;
             }
-            isMoving = true;
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {
-            movement.x += horizontalMoveSpeed;
-            if (currentAnimation != "moveRight") {
-                currentAnimation = "moveRight";
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {
+                movement.x += horizontalMoveSpeed;
+                if (currentAnimation != "moveRight") {
+                    currentAnimation = "moveRight";
+                }
+                isMoving = true;
             }
-            isMoving = true;
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
-            movement.y -= verticalMoveSpeed;
-            if (currentAnimation != "moveUp") {
-                currentAnimation = "moveUp";
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
+                movement.y -= verticalMoveSpeed;
+                if (currentAnimation != "moveUp") {
+                    currentAnimation = "moveUp";
+                }
+                isMoving = true;
             }
-            isMoving = true;
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
-            movement.y += verticalMoveSpeed;
-            if (currentAnimation != "moveDown") {
-                currentAnimation = "moveDown";
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
+                movement.y += verticalMoveSpeed;
+                if (currentAnimation != "moveDown") {
+                    currentAnimation = "moveDown";
+                }
+                isMoving = true;
             }
-            isMoving = true;
+
+            if (!isMoving && !isAttacking && currentAnimation != "idle") {
+                currentAnimation = "idle";
+            }
         }
 
-        if (!isMoving && currentAnimation != "idle") {
-            currentAnimation = "idle";
+        // Detect left mouse click and trigger hit animation based on the direction of the mouse
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) {
+            sf::Vector2i mousePosition = sf::Mouse::getPosition();
+            sf::Vector2f characterPosition = getPosition();
+            float angle = atan2(mousePosition.y - characterPosition.y, mousePosition.x - characterPosition.x);
+
+            if (angle > -M_PI_4 && angle < M_PI_4) {
+                hit("right");
+            } else if (angle > M_PI_4 || angle < -M_PI_4) {
+                hit("left");
+            }
+            isAttacking = true;
+        } else {
+            isAttacking = false;
         }
 
         sf::Vector2f newPos = getPosition() + movement;
@@ -49,5 +101,37 @@ sf::Vector2f Slime::handleMovement(const TileMap& map) {
     } catch (const std::exception& e) {
         std::cerr << "An error occurred while handling movement: " << e.what() << "\n";
         return {0.f, 0.f};
+    }
+}
+
+void Slime::setTexture(const std::string &animationName, const sf::Vector2f &scale) {
+    if (animationName == "hitLeft") {
+        sprite.setTexture(hitLeftTexture);
+    } else if (animationName == "hitRight") {
+        sprite.setTexture(hitRightTexture);
+        sprite.setScale(scale);
+    } else if (animationName == "idle") {
+        sprite.setTexture(idleTexture);
+    } else if (animationName == "death") {
+        sprite.setTexture(deathTexture);
+    } else if (animationName == "moveDown") {
+        sprite.setTexture(moveDownTexture);
+    } else if (animationName == "moveRight") {
+        sprite.setTexture(moveRightTexture);
+        sprite.setScale(scale);
+    } else if (animationName == "moveLeft") {
+        sprite.setTexture(moveLeftTexture);
+    } else if (animationName == "moveUp") {
+        sprite.setTexture(moveUpTexture);
+    }
+}
+
+void Slime::setupAnimation(sf::Texture &texture, const std::string &animationName,
+                           const std::string &filePath, sf::Vector2i frameCount,
+                           sf::Vector2i frameSize, sf::Vector2i startPosition, int frequency) {
+    if (!texture.loadFromFile(filePath)) {
+        std::cerr << "Failed to load texture: " << filePath << "\n";
+    } else {
+        AnimationManager::addAnimation(animationName, texture, frameCount, frameSize, startPosition, frequency);
     }
 }
--- src/slime.h
+++ src/slime.h
@@ -2,37 +2,17 @@
 #ifndef slime_H
 #define slime_H
 #include <SFML/Graphics.hpp>
+#include <SFML/Window/Mouse.hpp>
 #include "AnimationManager.h"
 #include "tileMap.h"
 #include <iostream>
 
 class Slime {
 public:
-    Slime() : sprite(idleTexture), currentAnimation("idle") {
-        setupAnimation(hitTexture, "hit", "assets/slime/hit/damage.png", {7, 1}, {40, 32}, {0, 0}, 6);
-        setupAnimation(idleTexture, "idle", "assets/slime/Idle/idle.png", {7, 1}, {30, 27}, {0, 0}, 10);
-        setupAnimation(deathTexture, "death", "assets/slime/Death/death.png", {6, 1}, {55, 45}, {0, 0}, 4);
-
-        // Movement animations
-        setupAnimation(moveDownTexture, "moveDown", "assets/slime/walking/down/walkingDown.png", {3, 1}, {42, 42},
-                       {0, 0}, 7);
-        setupAnimation(moveRightTexture, "moveRight", "assets/slime/walking/sideways/walkingRight.png", {7, 1},
-                       {30, 28}, {0, 0}, 6);
-        setupAnimation(moveLeftTexture, "moveLeft", "assets/slime/walking/sideways/walkingLeft.png", {7, 1}, {30, 28},
-                       {0, 0}, 6);
-        setupAnimation(moveUpTexture, "moveUp", "assets/slime/walking/up/walkingUpwards.png", {3, 1}, {43, 42}, {0, 0},
-                       7);
-        sprite.setTexture(idleTexture);
-    }
-
-    void draw(sf::RenderWindow &window) {
-        AnimationManager::update(currentAnimation, sprite);
-        window.draw(sprite);
-    }
-
-    void move(const sf::Vector2f &offset) {
-        sprite.move(offset);
-    }
+    Slime();
+    void draw(sf::RenderWindow &window);
+    void move(const sf::Vector2f &offset);
+    void hit(const std::string &direction);
 
     void setPosition(const sf::Vector2f &position) {
         sprite.setPosition(position);
@@ -50,7 +30,8 @@ public:
 
 private:
     sf::Sprite sprite;
-    sf::Texture hitTexture;
+    sf::Texture hitRightTexture;
+    sf::Texture hitLeftTexture;
     sf::Texture idleTexture;
     sf::Texture deathTexture;
     sf::Texture moveDownTexture;
@@ -58,34 +39,11 @@ private:
     sf::Texture moveLeftTexture;
     sf::Texture moveUpTexture;
     std::string currentAnimation;
+    bool isAttacking = false;
 
-    void setTexture(const std::string &animationName, const sf::Vector2f &scale) {
-        if (animationName == "hit") {
-            sprite.setTexture(hitTexture);
-        } else if (animationName == "idle") {
-            sprite.setTexture(idleTexture);
-        } else if (animationName == "death") {
-            sprite.setTexture(deathTexture);
-        } else if (animationName == "moveDown") {
-            sprite.setTexture(moveDownTexture);
-        } else if (animationName == "moveRight") {
-            sprite.setTexture(moveRightTexture);
-            sprite.setScale(scale);
-        } else if (animationName == "moveLeft") {
-            sprite.setTexture(moveLeftTexture);
-        } else if (animationName == "moveUp") {
-            sprite.setTexture(moveUpTexture);
-        }
-    }
-
+    void setTexture(const std::string &animationName, const sf::Vector2f &scale);
     static void setupAnimation(sf::Texture &texture, const std::string &animationName,
                                const std::string &filePath, sf::Vector2i frameCount,
-                               sf::Vector2i frameSize, sf::Vector2i startPosition, int frequency) {
-        if (!texture.loadFromFile(filePath)) {
-            std::cerr << "Failed to load texture: " << filePath << "\n";
-        } else {
-            AnimationManager::addAnimation(animationName, texture, frameCount, frameSize, startPosition, frequency);
-        }
-    }
+                               sf::Vector2i frameSize, sf::Vector2i startPosition, int frequency);
 };
 #endif
