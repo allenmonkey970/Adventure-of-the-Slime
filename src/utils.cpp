8f0f32c640ebcb09f75358bc79ebbf95f468d762
antho <anthonyzolmora@outlook.com>
2025-02-18 23:14:03 -0700

    added mac os option for game saves

--- src/utils.cpp
+++ src/utils.cpp
@@ -4,13 +4,15 @@
 #ifdef _WIN32
 #include <windows.h>
 #include <shlobj.h>
+#elif defined(__APPLE__)
+#include <unistd.h>
+#include <pwd.h>
+#include <sys/types.h>
 #else
 #include <unistd.h>
 #include <pwd.h>
 #endif
 
-
-// add mac os option
 std::string GetLocalPath() {
 #ifdef _WIN32
     char localPath[MAX_PATH];
@@ -18,6 +20,14 @@ std::string GetLocalPath() {
         return {localPath};
     }
     throw std::runtime_error("Failed to get Local AppData path.");
+#elif defined(__APPLE__)
+    const char* homedir = getenv("HOME");
+    if (!homedir) {
+        homedir = getpwuid(getuid())->pw_dir;
+    }
+    std::string localShare(homedir);
+    localShare.append("/Library/Application Support");
+    return localShare;
 #else
     const char* homedir;
     if ((homedir = getenv("HOME")) == nullptr) {
